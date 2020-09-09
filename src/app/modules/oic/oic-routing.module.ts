aa2f9476e21e6c787a752e57fdf5f05c26bf76d2
Girjesh kumar Suryawanshi <girjeshsuryawanshi@gmail.com>
2020-09-09 13:34:04 +0530

    home component added

--- src/app/modules/oic/oic-routing.module.ts
+++ src/app/modules/oic/oic-routing.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OicComponent } from './oic.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { AddIssueComponent } from './add-issue/add-issue.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path:'', component:OicComponent,
     children: [
       {
+          path: 'home', component: HomeComponent
+        },
+  
+        {
         path: '',
         children: [
-          {path:'add',component:AddUserComponent}
+          {path:'add-user',component:AddUserComponent}
+        ]
+      },
+      {
+        path: '',
+        children: [
+          {path:'add-issue',component:AddIssueComponent}
         ]
       }
     ]
--- src/app/modules/oic/oic.module.ts
+++ src/app/modules/oic/oic.module.ts
@@ -7,12 +7,14 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { AddIssueComponent } from './add-issue/add-issue.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 
 @NgModule({
-  declarations: [OicComponent,NavbarComponent,SidebarComponent, AddUserComponent,FooterComponent],
+  declarations: [OicComponent,NavbarComponent,SidebarComponent, AddUserComponent,FooterComponent, AddIssueComponent, HomeComponent],
   imports: [
     CommonModule,
     OicRoutingModule
