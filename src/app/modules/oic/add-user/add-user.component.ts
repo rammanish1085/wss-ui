f9768fcf58bf012af960383a1c0e4569962ff19b
girjesh-suryawanshi <girjeshsuryawanshi@gmail.com>
2020-10-17 19:51:42 +0530

    User delete with confirmation message done

--- src/app/modules/oic/add-user/add-user.component.ts
+++ src/app/modules/oic/add-user/add-user.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthorizationService } from 'src/app/services/authorization-service/authorization.service';
-import {UserService} from 'src/app/services/users/user.service';
-import {ProjectUserMappingService} from 'src/app/services/project/project-user-mapping.service';
-import {GobalutilityService} from 'src/app/utility/gobalutility.service';
+import { UserService } from 'src/app/services/users/user.service';
+import { ProjectUserMappingService } from 'src/app/services/project/project-user-mapping.service';
+import { GobalutilityService } from 'src/app/utility/gobalutility.service';
 import { User } from 'src/app/models/user.model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ConstantPool } from '@angular/compiler';
 
 @Component({
   selector: 'app-add-user',
@@ -13,16 +14,16 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AddUserComponent implements OnInit {
 
-  project:any;
-  users : any;
-  projectUserMappingObject :any ={};
+  project: any;
+  users: any;
+  projectUserMappingObject: any = {};
   loggedInUser: User;
-  projectUserMapping:any;
-  locationCode :string;
+  projectUserMapping: any;
+  locationCode: string;
 
   userProjectFrom: FormGroup;
 
-  constructor(private userService:UserService,private authorizationService: AuthorizationService,private projectUserMappingService:ProjectUserMappingService,private globalUtilityService:GobalutilityService) { }
+  constructor(private userService: UserService, private authorizationService: AuthorizationService, private projectUserMappingService: ProjectUserMappingService, private globalUtilityService: GobalutilityService) { }
 
   ngOnInit() {
 
@@ -37,77 +38,111 @@ export class AddUserComponent implements OnInit {
     this.getProject();
     this.getUsers();
     this.getAssignProject();
-    }
+  }
 
   public getProject() {
     console.log('Getting Data Called');
-    this.userService.getAllProject().subscribe(succes=>{
-      console.log("succes");
+    this.userService.getAllProject().subscribe(succes => {
+      console.log('succes');
       console.log(succes.body);
-      this.project=succes.body;
-    },error=>{
-      console.log("error");
+      this.project = succes.body;
+    }, error => {
+      console.log('error');
       console.log(error);
 
     });
-    
+
   }
 
-  private getUsers(){
+  private getUsers() {
     console.log('Getting USers');
-    this.userService.getAllUsers(this.locationCode,'oic').subscribe(success=>{
+    this.userService.getAllUsers(this.locationCode, 'oic').subscribe(success => {
       console.log(success.body);
       this.users = success.body;
-    },()=>{
-     
+    }, () => {
+
       // this.project=succes.body;
 
     });
 
   }
 
-  onclickAddUserProject(){
-   console.log("Getting form value");
+  onclickAddUserProject() {
+    console.log('Getting form value');
 
-   console.log(this.userProjectFrom.value);
+    console.log(this.userProjectFrom.value);
 
-   this.prepareProjectUserMapping();
-     this.projectUserMappingService.insertProjectUserMapping(this.projectUserMappingObject).subscribe(success=>{
-       this.globalUtilityService.successAlertMessage("Project assigned successfully !!!")
-       this.reset();
-       this.getAssignProject();
-     },()=>{
-      this.globalUtilityService.errorAlertMessage("Project already exist for selected user !!!");
-       this.reset();
-     });
+    this.prepareProjectUserMapping();
+    this.projectUserMappingService.insertProjectUserMapping(this.projectUserMappingObject).subscribe(success => {
+      this.globalUtilityService.successAlertMessage('Project assigned successfully !!!');
+      this.reset();
+      this.getAssignProject();
+    }, () => {
+      this.globalUtilityService.errorAlertMessage('Project already exist for selected user !!!');
+      this.reset();
+    });
 
-     }
+  }
 
-  prepareProjectUserMapping(){
-    this.projectUserMappingObject.username=this.userProjectFrom.value.user.username;
-    this.projectUserMappingObject.name =this.userProjectFrom.value.user.name;
-    this.projectUserMappingObject.locationCode=this.loggedInUser.getLocationCode();
-    this.projectUserMappingObject.locationName=this.loggedInUser.getLocationShortName();
-    this.projectUserMappingObject.projectName=this.userProjectFrom.value.project.name;
-    this.projectUserMappingObject.deleted=false;
-    this.projectUserMappingObject.createdBy=this.loggedInUser.getUsername() +" "+ this.loggedInUser.getName();
-  }   
+  prepareProjectUserMapping() {
+    this.projectUserMappingObject.username = this.userProjectFrom.value.user.username;
+    this.projectUserMappingObject.name = this.userProjectFrom.value.user.name;
+    this.projectUserMappingObject.locationCode = this.loggedInUser.getLocationCode();
+    this.projectUserMappingObject.locationName = this.loggedInUser.getLocationShortName();
+    this.projectUserMappingObject.projectName = this.userProjectFrom.value.project.name;
+    this.projectUserMappingObject.deleted = false;
+    this.projectUserMappingObject.createdBy = this.loggedInUser.getUsername() + ' ' + this.loggedInUser.getName();
+  }
 
-getAssignProject(){
+  getAssignProject() {
 
-  this.projectUserMappingService.getAssignedProjectList(this.loggedInUser.getLocationCode()).subscribe(success=>{
-    this.projectUserMapping =success.body;
-    console.log("Inside ger Assigned Project");
-    console.log(this.projectUserMapping);
-  },()=>{
-  });
+    this.projectUserMappingService.getAssignedProjectList(this.loggedInUser.getLocationCode()).subscribe(success => {
+      this.projectUserMapping = success.body;
+      console.log('Inside ger Assigned Project');
+      console.log(this.projectUserMapping);
+    }, () => {
+    });
 
-}
-reset(){
+  }
+
+  onClickDeleteUser(project: any, index: any) {
+    console.log('Onclick Called');
+    let confirmAlertResponse = this.globalUtilityService.confirmAlertMessage("Do you want to delete User ?");
+    confirmAlertResponse.then((result) => {
+      if (result.value) {
+        this.deleteUser(project);
+      }
+    });
+
+  }
+
+  deleteUser(project: any) {
+
+    let index = this.projectUserMapping.indexOf(project);
+
+    this.userService.deleteById(project.id).subscribe(success => {
+
+
+      console.log("inside success");
+
+      console.log(success);
+
+      console.log(success.body);
+
+      if (success.status === 200) {
+        this.globalUtilityService.successAlertMessage("User Deleted Successfully.");
+        this.projectUserMapping.splice(index, 1);
+      }
+    }, error => { })
+
+  }
+
+
+  reset() {
     this.userProjectFrom.patchValue({
-    project: '',
-    user:''
-  });
-}
+      project: '',
+      user: ''
+    });
+  }
 
 }
--- src/app/services/users/user.service.ts
+++ src/app/services/users/user.service.ts
@@ -8,25 +8,34 @@ import { GlobalConfiguration } from 'src/app/config/global.config';
 })
 export class UserService {
 
-  contextPath:any; 
-  constructor(private http :HttpClient,private globalConfiguration:GlobalConfiguration) {
-    this.contextPath=this.globalConfiguration.getBackendURLPrefix();
-   }
+  contextPath: any;
+  constructor(private http: HttpClient, private globalConfiguration: GlobalConfiguration) {
+    this.contextPath = this.globalConfiguration.getBackendURLPrefix();
+  }
 
 
-  getAllProject(){
+  getAllProject() {
     console.log("Inside Location Code and Name Service");
-    return this.http.get(this.contextPath+'/users',{observe: 'response'}).pipe(map(
-       (response : HttpResponse<any>)=>{
-        return response;}));  
-    }
+    return this.http.get(this.contextPath + '/users', { observe: 'response' }).pipe(map(
+      (response: HttpResponse<any>) => {
+        return response;
+      }));
+  }
 
 
 
-    getAllUsers(locationCode:string,role:any){
-      console.log("Inside getting users");
-      return this.http.get(this.contextPath+'/users-detail/location-code/'+locationCode +'/role/'+role,{observe: 'response'}).pipe(map(
-         (response : HttpResponse<any>)=>{
-          return response;}));  
-      }
+  getAllUsers(locationCode: string, role: any) {
+    console.log("Inside getting users");
+    return this.http.get(this.contextPath + '/users-detail/location-code/' + locationCode + '/role/' + role, { observe: 'response' }).pipe(map(
+      (response: HttpResponse<any>) => {
+        return response;
+      }));
+  }
+
+
+  deleteById(id) {
+    return this.http.delete(this.contextPath + "project-user-mapping/id/" + id, { observe: 'response' }).pipe(map((response: HttpResponse<any>) => {
+      return response;
+    }));
+  }
 }
