738d6cff7210a80b494d628e14168f581438d96f
girjesh-suryawanshi <girjeshsuryawanshi@gmail.com>
2021-03-21 22:54:44 +0530

    Add incident component created

--- src/app/modules/oic/add-incident/add-incident.component.ts
+++ src/app/modules/oic/add-incident/add-incident.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { GobalutilityService } from 'src/app/utility/gobalutility.service';
 
 @Component({
   selector: 'app-add-incident',
@@ -21,9 +22,11 @@ export class AddIncidentComponent implements OnInit {
 
   isDepartment: boolean;
 
+  myFiles: any[];
 
 
-  constructor() { }
+
+  constructor(private globalutilityService: GobalutilityService) { }
 
   incidentMasterForm: FormGroup;
 
@@ -34,7 +37,8 @@ export class AddIncidentComponent implements OnInit {
       incidentObject: new FormControl('', Validators.required),
       incidentobjecType: new FormControl('', Validators.required),
       employeeType: new FormControl('', Validators.required),
-      incidentDescription : new FormControl('', Validators.required)
+      incidentDescription : new FormControl('', Validators.required),
+      isAttachment:new FormControl(false)
     });
   }
 
@@ -43,9 +47,11 @@ export class AddIncidentComponent implements OnInit {
     console.log(this.incidentMasterForm.value);
     if (this.incidentMasterForm.value.incidentObject == 'Human') {
       this.isHuman = true;
+                  
     } else {
       this.isHuman = false;
-      this.isDepartment = false;
+      this.isDepartment = false;     
+
     }
 
 
@@ -60,9 +66,67 @@ export class AddIncidentComponent implements OnInit {
     console.log(this.incidentMasterForm.value.incidentobjecType);
     if (this.incidentMasterForm.value.incidentobjecType == 'Department') {
       this.isDepartment = true;
+      
     } else {
       this.isDepartment = false;
+           
     }
+
+  }
+
+  isAttachmentClicked() {
+    this.incidentMasterForm.get('isAttachment').valueChanges.subscribe(checked => {
+      if (checked) {
+        const validators = [Validators.required];
+        this.incidentMasterForm.addControl('attachment', new FormControl('', validators));
+      } else {
+        this.incidentMasterForm.removeControl('attachment');
+      }
+
+    });
+  }
+
+
+  deleteFieldValue(index) {
+    if (this.myFiles.length <= 1) {
+      this.myFiles.splice(index, 1);
+      this.resetFile();
+    } else {
+      this.myFiles.splice(index, 1);
+    }
+  }
+
+
+
+  onFileChange(event) {
+
+    this.myFiles = [];
+
+    const size = event.srcElement.files[0].size;
+
+    console.log(size)
+
+    if (size < 1000000) 
+    { 
+     if(event.target.files.length <=5){
+           
+      for (var i = 0; i < event.target.files.length; i++) {
+        this.myFiles.push(event.target.files[i]);
+      }
+    } else{
+        this.globalutilityService.errorAlertMessage("Maximum 5 File Allow to upload");
+      }
+
+    }else{
+    this.globalutilityService.errorAlertMessage("File Size greater 1 Mb");
+    }
+
+  }
+
+  resetFile() {
+    this.incidentMasterForm.patchValue({
+      attachment: '',
+    });
 
   }
 
